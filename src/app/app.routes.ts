662e654662c195dea8cb47b987ebb01d55e0ce6c
tesisbot <tesisbot@gmail.com>
2025-04-28 02:34:08 +0000

    WE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { BluetoothComponent } from './Components/bluetooth/bluetooth.component';
 import { UbicacionComponent } from './Components/ubicacion/ubicacion.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'bluetooth', component: BluetoothComponent },
   { path: 'ubicacion', component: UbicacionComponent },
   { path: '**', redirectTo: '' }
