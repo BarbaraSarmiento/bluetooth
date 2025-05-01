f34ed5e70f88222147ce4be6dfce7df10c97d283
tesisbot <tesisbot@gmail.com>
2025-05-01 21:23:39 +0000

    Talvez

--- android/app/src/main/java/com/uets/blutu/MainActivity.java
+++ android/app/src/main/java/com/uets/blutu/MainActivity.java
@@ -2,4 +2,4 @@ package com.uets.blutu;
 
 import com.getcapacitor.BridgeActivity;
 
-public class ChuasBot extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {}
--- android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -1,3 +1,4 @@
+//android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
 package com.getcapacitor.myapp;
 
 import static org.junit.Assert.*;
