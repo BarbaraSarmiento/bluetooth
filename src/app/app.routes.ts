07b09b9d97464e589d46931b8d854698ea90acba
tesisbot <tesisbot@gmail.com>
2025-05-01 19:52:08 +0000

    Name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
-//src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { BluetoothComponent } from './Components/bluetooth/bluetooth.component';
 import { UbicacionComponent } from './Components/ubicacion/ubicacion.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta raíz redirige a home
   { path: 'home', component: HomeComponent },
-  { path: 'almacenamiento', component: BluetoothComponent }, // Cambia esto si prefieres 'almacenamiento'
+  { path: 'bluetooth', component: BluetoothComponent }, // Cambia esto si prefieres 'almacenamiento'
   { path: 'ubicacion', component: UbicacionComponent },
   { path: '**', redirectTo: 'home' } // Redirige rutas no encontradas a home
 ];
