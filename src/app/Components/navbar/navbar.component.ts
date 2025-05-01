9f4718a24b2137903e637b91e451f51289ca0526
tesisbot <tesisbot@gmail.com>
2025-05-01 16:51:47 +0000

    PruebaNav

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,32 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { BluetoothService } from '../bluetooth.service';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  buttons = [
-    { text: 'Home', icon: 'assets/images/home-icon.png', path: '/home' },
-    { text: 'Ubicación', icon: 'assets/images/ubicacion-icon.png', path: '/ubicacion' },
-    { text: 'Almacenamiento', icon: 'assets/images/bluetooth-icon.png', path: '/bluetooth' }
-  ];
+export class NavbarComponent {
+  constructor(private router: Router) {}
 
-  logs: string[] = [];
-
-  constructor(private router: Router, private bluetoothService: BluetoothService) {}
-
-  navigateTo(path: string): void {
-    this.router.navigate([path]);
-  }
-
-  ngOnInit() {
-    // Puedes mantener la lógica de inicialización del BluetoothService si es necesaria
+  navigateTo(route: string) {
+    this.router.navigate([route]);
   }
 }
